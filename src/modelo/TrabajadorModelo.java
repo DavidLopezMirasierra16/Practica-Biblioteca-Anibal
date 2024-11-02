d1db21b888419e4b5631523ee46a041dcd72286b
DavidLopezMirasierra16 <dlm1652003@gmail.com>
2024-11-02 17:45:59 +0100

    s

--- src/modelo/TrabajadorModelo.java
+++ src/modelo/TrabajadorModelo.java
@@ -11,7 +11,7 @@ package modelo;
 public class TrabajadorModelo {
     
     public static void main(String[] args) {
-        System.out.println("Prueba 2");
+        System.out.println("Prueba 1");
     }
     
 }
