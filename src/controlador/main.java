a29ea641aea4e292f116dcb37fa04e5b66adc205
DavidLopezMirasierra16 <dlm1652003@gmail.com>
2024-11-03 01:44:39 +0100

    cambios controller 2

--- src/controlador/main.java
+++ src/controlador/main.java
@@ -3,6 +3,8 @@ package controlador;
 public class main {
     
     public static void main(String[] args) {
-        System.out.println("Este es el bueno");
+
+        new LoginController();
+
     }
 }
