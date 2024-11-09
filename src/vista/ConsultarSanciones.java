bbff7d25ff42c864c9a901f5fa83c1bfc967b67c
Pablondnd246 <pablopolaino@gmail.com>
2024-11-09 13:43:30 +0100

    Consultar Sanciones

--- src/vista/ConsultarSanciones.java
+++ src/vista/ConsultarSanciones.java
@@ -34,27 +34,22 @@ public class ConsultarSanciones extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         tablaSanciones = new javax.swing.JTable();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        txtBusqueda = new javax.swing.JTextField();
-        cbFiltro = new javax.swing.JComboBox<>();
-        btnBuscar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         tablaSanciones.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null}
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
             },
             new String [] {
-                "ID", "Nombre", "Apellidos", "Localidad", "Sanción", "Pago"
+                "ID", "Socio", "Prestamo", "Sancion"
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, false
+                false, false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
@@ -65,49 +60,21 @@ public class ConsultarSanciones extends javax.swing.JFrame {
         jScrollPane1.setViewportView(tablaSanciones);
         if (tablaSanciones.getColumnModel().getColumnCount() > 0) {
             tablaSanciones.getColumnModel().getColumn(0).setResizable(false);
+            tablaSanciones.getColumnModel().getColumn(1).setResizable(false);
             tablaSanciones.getColumnModel().getColumn(2).setResizable(false);
-            tablaSanciones.getColumnModel().getColumn(4).setResizable(false);
-            tablaSanciones.getColumnModel().getColumn(5).setResizable(false);
+            tablaSanciones.getColumnModel().getColumn(3).setResizable(false);
         }
-
-        jLabel1.setText("Filtrar Por");
-
-        jLabel2.setText("Introduzca el dato a buscar");
-
-        cbFiltro.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccione una opción", "Nombre", "Apellidos", "Localidad", "ID", "Pago" }));
-
-        btnBuscar.setText("Buscar");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 717, Short.MAX_VALUE)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(cbFiltro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(12, 12, 12)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(txtBusqueda, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(btnBuscar)
-                .addGap(52, 52, 52))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(18, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jLabel1)
-                        .addComponent(jLabel2)
-                        .addComponent(txtBusqueda, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(btnBuscar))
-                    .addComponent(cbFiltro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                .addContainerGap(59, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(55, 55, 55))
         );
@@ -150,22 +117,6 @@ public class ConsultarSanciones extends javax.swing.JFrame {
         });
     }
 
-    public JComboBox<String> getCbFiltro() {
-        return cbFiltro;
-    }
-
-    public JButton getBtnBuscar() {
-        return btnBuscar;
-    }
-
-    public void setBtnBuscar(JButton btnBuscar) {
-        this.btnBuscar = btnBuscar;
-    }
-    
-    public void setCbFiltro(JComboBox<String> cbFiltro) {
-        this.cbFiltro = cbFiltro;
-    }
-
     public JTable getTablaSanciones() {
         return tablaSanciones;
     }
@@ -173,24 +124,9 @@ public class ConsultarSanciones extends javax.swing.JFrame {
     public void setTablaSanciones(JTable tablaSanciones) {
         this.tablaSanciones = tablaSanciones;
     }
-
-    public JTextField getTxtBusqueda() {
-        return txtBusqueda;
-    }
-
-    public void setTxtBusqueda(JTextField txtBusqueda) {
-        this.txtBusqueda = txtBusqueda;
-    }
     
-    
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnBuscar;
-    private javax.swing.JComboBox<String> cbFiltro;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tablaSanciones;
-    private javax.swing.JTextField txtBusqueda;
     // End of variables declaration//GEN-END:variables
 }
